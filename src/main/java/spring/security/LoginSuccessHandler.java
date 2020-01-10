9284172f0807683192932649bdee034c41f03846
revseev <useless47@gmail.com>
2020-01-10 19:17:35 +0300

    minor cleanup

--- src/main/java/spring/security/LoginSuccessHandler.java
+++ src/main/java/spring/security/LoginSuccessHandler.java
@@ -24,8 +24,10 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     protected String determineTargetUrl(Authentication authentication) {
         boolean isUser = false;
         boolean isAdmin = false;
+
         Collection<? extends GrantedAuthority> authorities
                 = authentication.getAuthorities();
+
         for (GrantedAuthority grantedAuthority : authorities) {
             if (grantedAuthority.getAuthority().equals("ROLE_USER")) {
                 isUser = true;
